41e1fcf69c00c2f237bbf44979a07fb0d62dca2e
Vazgen Karapetyan <vkarapetyan@irobot.com>
2020-06-20 11:36:19 -0700

    Refactored the method start(), partially switched to await...

--- src/ms_job.js
+++ src/ms_job.js
@@ -48,21 +48,21 @@ class Job
     {
     }
 
-    start(board) 
+    docker_image()
     {
-        const job = this;
-        job.board = board;
-        job.start_timestamp = Date.now();
+        return this.params.docker_registry + '/' + this.params.docker_image;
+    }
 
-        //console.log(`[J] starting job [${LOG_JOB(job)}]`);
-        const docker_image = this.params.docker_registry + '/' + this.params.docker_image;
-        const session_name = this.params.session_name;
-        const job_id = this.params.uid;
+    async docker_containers_cleanup()
+    {
+        const board = this.board;
 
-        board.docker.listContainers({
-            all: true
-        }).then(function(containers) {
-            const promises = containers.map(function(cntr) {
+        const list_containers = board.docker.listContainers({
+                                                    all: true
+                                                });
+        const containers = await list_containers;
+
+        const promises = containers.map(function(cntr) {
                     console.log(`[J] stopping container[${LOG_CONTAINER(cntr.Id)}]....`);
                     const container = board.docker.getContainer(cntr.Id);
                     return container.stop()
@@ -78,82 +78,131 @@ class Job
                     });
                 });
 
-            return Promise.all(promises);
-        }).then(function() {
-            console.log(`[J] container cleanup for board[${LOG_BOARD(board)}] is done, pulling the requested image [${docker_image}]`);
-            return new Promise(function(resolve, reject) {
-                board.docker.pull(
-                    docker_image,
-                    function (err, stream) {
-                    if (err) {
-                        console.log(ERROR(`[J] failed to start exec modem: ${err}`));
-                        return reject();
-                    }
-                    let message = '';
-                    if(err) return reject(err);
-                    stream.on('data', data => { message += data });
-                    stream.on('end', () => resolve(message));
-                    stream.on('error', err => reject(err));
-                });
-            });        
-        }).then(function(message){
-            console.log(`[J] image ready for [${LOG_BOARD(board)}].`);
-            return board.docker.createContainer({
-                Image: docker_image,
-                name: `session-${session_name}.job-${job_id}`,
-                Cmd: ['/bin/bash'],
-                Tty: true,
-                HostConfig: {
-                    AutoRemove: true
-                }});
-        }).then(function(container) {
-            console.log(`[J] starting container for job[${LOG_JOB(job)}] on board[${LOG_BOARD(board)}].`);
-            job.container = container;
-            return job.container.start();
-        }).then(function(data) {
-            console.log(`[J] container[${LOG_CONTAINER(job.container.id)}] is created for job[${LOG_JOB(job)}].`);
-            return job.container.exec({
-                    Cmd: job.params.command,
-                    AttachStdout: true,
-                    AttachStderr: true,
-                    Tty: true
-            });
-        }).then(function(exec) {
-            return new Promise(function(exec_resolve, exec_reject) {
-                exec.start((err, stream) => {
-                    if (err) {
-                        console.log(ERROR('[J] failed to start exec modem...'));
-                        return reject();
-                    }
-                    const out_stream = fs.createWriteStream(job.params.out_file);
-                    job.container.modem.demuxStream(stream, out_stream, out_stream);
-                    new Promise(function(resolve, reject) {
+        await Promise.all(promises);
+    }
 
-                        let message = '';
-                        stream.on('data', data => {  message += data});
-                        stream.on('end', function () { 
-                            console.log(`[J] stream from job[${LOG_JOB(job)}] ended.`);
-                            exec.inspect(function(err, data) {
-                                if (!err && !data.Running) {
-                                    console.log(`[J] job[${LOG_JOB(job)}] exited with code ${data.ExitCode}`);
-		                            //console.log('MSG',message);
-                                    //reject();
-                                }
-                            });
-                            resolve(); 
+    async docker_image_pull() 
+    {
+        const job = this;
+        const board = this.board;
+
+        const pull = new Promise(function(resolve, reject) {
+            board.docker.pull(
+                job.docker_image(),
+                function (err, stream) {
+                if (err) {
+                    console.log(ERROR(`[J] failed to start exec modem: ${err}`));
+                    return reject();
+                }
+                let message = '';
+                if(err) return reject(err);
+                stream.on('data', data => { message += data });
+                stream.on('end', () => resolve(message));
+                stream.on('error', err => reject(err));
+            });
+        }); 
+        await pull;
+    }
+
+    async docker_container_run()
+    {
+        const job = this;
+        const board = job.board;
+        const job_id = this.params.uid;
+        const session_name = this.params.session_name;
+
+        const container = await board.docker.createContainer({
+                                            Image: job.docker_image(),
+                                            name: `session-${session_name}.job-${job_id}`,
+                                            Cmd: ['/bin/bash'],
+                                            Tty: true,
+                                            HostConfig: {
+                                                AutoRemove: true
+                                            }
+                                        });
+
+        console.log(`[J] starting container for job[${LOG_JOB(job)}] on board[${LOG_BOARD(board)}].`);
+        job.container = container;
+        await job.container.start();
+    }
+
+    async docker_container_exec()
+    {
+        const job = this;
+        const exec = await job.container.exec({
+                                    Cmd: job.params.command,
+                                    AttachStdout: true,
+                                    AttachStderr: true,
+                                    Tty: true
+                                 });
+                        
+        const stream_start =  new Promise(function(exec_resolve, exec_reject) {
+            exec.start((err, stream) => {
+                if (err) {
+                    console.log(ERROR('[J] failed to start exec modem...'));
+                    return reject();
+                }
+                const out_stream = fs.createWriteStream(job.params.out_file);
+                job.container.modem.demuxStream(stream, out_stream, out_stream);
+                new Promise(function(resolve, reject) {
+
+                    let message = '';
+                    stream.on('data', data => {  message += data});
+                    stream.on('end', function () { 
+                        console.log(`[J] stream from job[${LOG_JOB(job)}] ended.`);
+                        exec.inspect(function(err, data) {
+                            if (!err && !data.Running) {
+                                console.log(`[J] job[${LOG_JOB(job)}] exited with code ${data.ExitCode}`);
+                                //console.log('MSG',message);
+                                //reject();
+                            }
                         });
-                    }).finally(function(data) {
-                        exec_resolve();
+                        resolve(); 
                     });
+                }).finally(function(data) {
+                    exec_resolve();
                 });
             });
-        }).catch(function(err) {
-            console.log(ERROR(`[J] failed to exec the container for job[${LOG_JOB(job)}] on board[${LOG_BOARD(board)}]...`));
-            // TBD: possible error: err { Error: (HTTP code 400) unexpected - No exec command specified 
-            console.log('err', err);
-        }).finally(function() {
-            job.stop_container();
         });
+
+        try {
+            await stream_start;
+        } catch(e) {
+            console.log(ERROR('stream exec error: ', err));
+        };
+    }
+
+    async start(board) 
+    {
+        const job = this;
+        job.board = board;
+        job.start_timestamp = Date.now();
+
+        await job.docker_containers_cleanup();
+        
+        console.log(`[J] container cleanup for board[${LOG_BOARD(board)}] is done, pulling the requested image [${this.docker_image()}]`);
+        try {
+            await job.docker_image_pull();
+        } catch(e) {
+            console.log(ERROR(`[J] failed to pull image [${this.docker_image()}] on board[${LOG_BOARD(board)}]...`));
+            return;
+        }
+
+        console.log(`[J] image ready for [${LOG_BOARD(board)}], running...`);
+        try {
+            await job.docker_container_run();
+        } catch(e) {
+            console.log(ERROR(`[J] Failed to start the container for job[${LOG_JOB(job)}], ${e}`));
+        };
+
+
+        console.log(`[J] container[${LOG_CONTAINER(job.container.id)}] is created for job[${LOG_JOB(job)}].`);
+        try {
+            await job.docker_container_exec();
+        } catch(e) {
+            console.log(ERROR(`[J] failed to exec the container for job[${LOG_JOB(job)}] on board[${LOG_BOARD(board)}]...`));
+        }
+        await job.stop_container();
     }
 };
 
