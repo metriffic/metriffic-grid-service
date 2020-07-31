ab82aecadfb8280acf47f0c13efc98e713e1a5f8
Vazgen Karapetyan <vkarapetyan@irobot.com>
2020-07-31 10:43:13 -0700

    Using env variable getter library
    Allows type conversion, validation, etc.

--- src/config.js
+++ src/config.js
@@ -1,17 +1,19 @@
 const path   = require('path');
+const env = require('env-var');
 
 module.exports = {
-    GQL_ADDRESS: process.env['METRIFFIC_GQL_ADDRESS'],
+    GQL_ADDRESS: env.get('METRIFFIC_GQL_ADDRESS').required().asString(),
 
     GQL_ENDPOINT: "grid_service",
-    
-    SSH_PORT_HOST_MIN: process.env['METRIFFIC_SSH_PORT_HOST_MIN'],
-    SSH_PORT_HOST_MAX: process.env['METRIFFIC_SSH_PORT_HOST_MAX'],
+
+    SSH_PORT_HOST_MIN: env.get('METRIFFIC_SSH_PORT_HOST_MIN').required().asIntPositive(),
+    SSH_PORT_HOST_MAX: env.get('METRIFFIC_SSH_PORT_HOST_MAX').required().asIntPositive(),
 
     // TBD: this is the copy of the const stored in metriffic-workspaces service...
-    USERSPACE_DIR_ROOT : process.env['METRIFFIC_USERSPACE_NFS_ROOT'],
-    USERSPACE_HOST : process.env['METRIFFIC_USERSPACE_NFS_HOST'],
+    USERSPACE_DIR_ROOT : env.get('METRIFFIC_USERSPACE_NFS_ROOT').required().asString(),
+    USERSPACE_HOST : env.get('METRIFFIC_USERSPACE_NFS_HOST').required().asString(),
 
-    GRID_SERVICE_PRIVATE_KEY_FILE: process.env['METRIFFIC_GRID_SERVICE_PRIVATE_KEY_FILE'],
+    DOCKER_REGISTRY_HOST : env.get('METRIFFIC_DOCKER_REGISTRY_HOST').required().asString(),
+    
+    GRID_SERVICE_PRIVATE_KEY_FILE: env.get('METRIFFIC_GRID_SERVICE_PRIVATE_KEY_FILE').required().asString(),
 }
-  
